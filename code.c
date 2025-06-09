acc607960b7833696bf85e2dd4d1c974b918700f
Pinky Sherwani <69427575+PINKY0212@users.noreply.github.com>
2025-06-09 16:52:10 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,24 +1,77 @@
 // C Program for line follower robot using 8051 microcontroller
 #include<reg51.h>
-sbit ls=P0^0;
-sbit rs=P0^1;
-#define motor P2
-#define forward 0x06
-#define turn_left 0x82
-#define turn_right 0x14
-#define stop 0x00
+sbit mot1=P2^0;
+sbit mot2=P2^1;
+sbit mot3=P2^2;
+sbit mot4=P2^3;
+
+sbit s_left=P2^6;
+sbit s_right=P2^7;
+
+void forward (void);
+void backward (void);
+void left (void);
+void right (void);
+
+void forward (void)
+{
+  mot1=0;
+	mot2=1;
+	mot3=1;
+	mot4=0;
+}
+void backward (void)
+{
+  mot1=0;
+	mot2=1;
+	mot3=0;
+	mot4=1;
+}
+void left (void)
+{
+  mot1=0;
+	mot2=1;
+	mot3=0;
+	mot4=0;
+}
+void right (void)
+{
+  mot1=0;
+	mot2=0;
+	mot3=1;
+	mot4=0;
+}
+void stop (void)
+{
+  mot1=0;
+	mot2=0;
+	mot3=0;
+	mot4=0;
+}
+
+
+
 void main()
 {
-    motor=stop;
-    while(1)
-    {
-     if(ls && rs)
-         motor=forward;
-     else if(!ls && rs)
-         motor=turn_left;
-     else if(ls && !rs)
-       motor=turn_right;
-     else 
-         motor=stop;
-  }
+	s_left=1;
+	s_right=1;
+	while(1)
+	{ 
+	 if(s_left==0 && s_right==0)
+	 {
+		 forward();
+	 }
+	 else if(s_left==1 && s_right==1)
+	 {
+		 stop();
+	 }
+	 else if(s_left==0 && s_right==1)
+	 {
+		 left();
+	 }
+	 else if(s_left==1 && s_right==0)
+	 {
+		 right();
+	 }
+ }
 }
